87a057ec134e0fd1dc9c6368ca8f7ca32e51975c
Paeythor <theschumann@byui.edu>
2025-06-06 17:07:44 -0600

    rafting

--- wwr/scripts.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    if (this.getAttribute('href').startsWith("#")) {
-      e.preventDefault();
-      const section = document.querySelector(this.getAttribute('href'));
-      section.scrollIntoView({ behavior: 'smooth' });
-    }
-  });
-});
